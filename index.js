d43714fa052bdad5dbe2bd68ab2d2a47b17ffc38
Patrick <p.ullrich@reply.de>
2021-03-20 15:08:23 +0100

    index.js update
    request implementation
    create about page route
    installation of request

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 // Stock Market Portfolio App By Patrick
-
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request')
 // Set the Port of the webhoster or on 5000
 const PORT = process.env.PORT || 5000;
+
+
+
+// API KEY pk_126fa11f22644aa3b471665f71e7220e
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_126fa11f22644aa3b471665f71e7220e', { json: true }, (err, res, body) => {
+    if (err) {return console.log(err);}
+    if (res.statusCode === 200){
+        console.log(body);
+    };
+});
+
+
+
 
 // Set Handlebars Middleware
 app.engine('handlebars', exphbs());
@@ -18,6 +31,11 @@ app.get('/', function (req, res) {
     res.render('home', {
         stuff: otherstuff
     });
+});
+
+// create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // Set static folder
