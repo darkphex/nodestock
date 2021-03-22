d08f50f33a606de5d426825d236a65f6bf3872ae
Patrick <p.ullrich@reply.de>
2021-03-22 20:03:31 +0100

    Update index.js, package.json, created procfile for heroku

--- index.js
+++ index.js
@@ -4,22 +4,25 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const request = require('request')
+const bodyParser = require('body-parser')
 // Set the Port of the webhoster or on 5000
 const PORT = process.env.PORT || 5000;
 
 
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
 // API KEY pk_126fa11f22644aa3b471665f71e7220e
 // create call_api function
-function call_api() {
-    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_126fa11f22644aa3b471665f71e7220e', { json: true }, (err, res, body) => {
+function call_api(finishedAPI, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_126fa11f22644aa3b471665f71e7220e', { json: true }, (err, res, body) => {
     if (err) {return console.log(err);}
     if (res.statusCode === 200){
         // console.log(body);
-        return body
-    };
-});
-}
+        finishedAPI(body);
+        };
+    });
+};
 
 
 
@@ -29,13 +32,25 @@ app.set('view engine', 'handlebars');
 
 const otherstuff = "hello there, this is other stuff!";
 
-//Set Handlebar routes
+//Set Handlebar index GET route
 app.get('/', function (req, res) {
-    const api = call_api();
-    console.log(api);
-    res.render('home', {
-        stock: api
-    });
+    call_api(function(doneAPI) {
+        res.render('home', {
+            stock: doneAPI
+        });
+    }, "fb");
+
+});
+
+//Set Handlebar POST route
+app.post('/', function (req, res) {
+    call_api(function(doneAPI) {
+        //posted_stuff = req.body.stock_ticker;
+        res.render('home', {
+            stock: doneAPI,
+        });
+    }, req.body.stock_ticker);
+
 });
 
 // create about page route
